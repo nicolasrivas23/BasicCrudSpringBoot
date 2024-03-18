d294e9f9228f27c8e69190ff508252f8e6e1360a
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-03-18 14:39:59 -0500

    Inicio del proyecto

---
+++ src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/BasicCrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.BasicCrudSpringBoot.BasicCrudSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/BasicCrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BasicCrudSpringBoot.BasicCrudSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
