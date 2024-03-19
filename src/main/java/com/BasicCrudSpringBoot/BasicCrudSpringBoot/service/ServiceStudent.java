ff84139737780d31ab2593445429d718236a0aaa
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-03-19 15:33:44 -0500

    Se solucionan errores de compilacion

--- src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/service/ServiceStudent.java
+++ src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/service/ServiceStudent.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public interface ServiceStudent {
     List<Student> findAll();
     Optional<Student> findById(Long id);
