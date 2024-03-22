3e860462b61b0c8b84b2ddbd577f027afa3b48d2
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-03-22 08:51:04 -0500

    Se soluciona error de importacion

--- src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/Service/ServiceStudent.java
+++ src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/Service/ServiceStudent.java
@@ -1,8 +1,8 @@
-package com.BasicCrudSpringBoot.BasicCrudSpringBoot.service;
+package com.BasicCrudSpringBoot.basiccrudspringboot.Service;
 
-import com.BasicCrudSpringBoot.BasicCrudSpringBoot.entity.Student;
-import org.springframework.stereotype.Service;
+import com.BasicCrudSpringBoot.basiccrudspringboot.entity.Student;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +10,6 @@ import java.util.Optional;
 public interface ServiceStudent {
     List<Student> findAll();
     Optional<Student> findById(Long id);
-    void saveOrUpdate(Student student);
+    void saveOrUpdate(Student student) throws IOException;
     void delete(Long id);
 }
--- src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/repository/StudentRepository.java
+++ src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/repository/StudentRepository.java
@@ -1,9 +1,10 @@
-package com.BasicCrudSpringBoot.BasicCrudSpringBoot.repository;
+package com.BasicCrudSpringBoot.basiccrudspringboot.repository;
 
-import com.BasicCrudSpringBoot.BasicCrudSpringBoot.entity.Student;
+import com.BasicCrudSpringBoot.basiccrudspringboot.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
