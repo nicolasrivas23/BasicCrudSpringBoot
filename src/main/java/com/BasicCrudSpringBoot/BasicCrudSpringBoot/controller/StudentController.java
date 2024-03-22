b028411288e9d9b96547fe7a2ba1009441cfacf5
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-03-22 09:14:17 -0500

    Controlador

--- src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/controller/StudentController.java
+++ src/main/java/com/BasicCrudSpringBoot/BasicCrudSpringBoot/controller/StudentController.java
@@ -1,37 +1,59 @@
-package com.BasicCrudSpringBoot.BasicCrudSpringBoot.controller;
+package com.BasicCrudSpringBoot.basiccrudspringboot.controller;
 
-import com.BasicCrudSpringBoot.BasicCrudSpringBoot.entity.Student;
-import com.BasicCrudSpringBoot.BasicCrudSpringBoot.service.Impl.StudentImpl;
+import com.BasicCrudSpringBoot.basiccrudspringboot.Service.Impl.StudentImpl;
+import com.BasicCrudSpringBoot.basiccrudspringboot.entity.Student;
+import com.BasicCrudSpringBoot.basiccrudspringboot.DTO.StudentDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@Slf4j
 public class StudentController {
 
     @Autowired
     private StudentImpl studentImpl;
 
     @GetMapping("/student")
-    @ResponseBody
     public List<Student> findAll(){
         return studentImpl.findAll();
     }
+
     @GetMapping("/studentById/{id}")
-    @ResponseBody
     public Optional<Student> findById(@PathVariable Long id){
         return studentImpl.findById(id);
     }
+
     @PostMapping
-    @ResponseBody
-    public void save(@RequestBody Student student){
+    public void save(@RequestBody Student student) throws IOException {
         studentImpl.saveOrUpdate(student);
     }
+
     @DeleteMapping(path = "{id}")
     public void delete(@PathVariable Long id){
         studentImpl.delete(id);
     }
+
+    @GetMapping("/ListarByIdS/{id}")
+    public ResponseEntity<StudentDTO> lista(@PathVariable Long id) throws Exception {
+        StudentDTO studentDTO = null;
+        try{
+            studentDTO = studentImpl.lista(id);
+            if (studentDTO != null){
+                return ResponseEntity.ok().body(studentDTO);
+            }else {
+                return ResponseEntity.noContent().build();
+            }
+        }catch (Exception ex){
+           log.error("ocurrio un error: " +ex.getMessage());
+        }
+      return null;
+    }
+
 }
